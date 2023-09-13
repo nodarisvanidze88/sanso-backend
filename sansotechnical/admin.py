ac80ddf991579393ead5eba9ff0f4bdb27c7ee7a
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2023-09-13 10:36:28 +0400

    #2 added models for admin panel

--- sansotechnical/admin.py
+++ sansotechnical/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register({
+    models.Alma_Systems, 
+    models.Alma_Systems_Models, 
+    models.Customers, 
+    models.HP_Details, 
+    models.HP_Models,
+    models.GivenItemsStatus,
+    models.CustomersHaveSystem,
+    models.customersHaveHP,
+    })
